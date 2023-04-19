fe29ad58df06a5170178af8a3522a22c7cebfd40
Yana Kozarenko <event2016.2017@gmail.com>
2023-04-19 20:43:57 +0100

    exersise 1 completed

---
+++ 3 - JS/Week 1/Exercise1/app.js
@@ -0,0 +1,10 @@
+//Exersise 1 - console.log - 1
+let hello = (num) => console.log(`${num * num} Hello, World!`);
+hello(58);
+
+//Exersise 1 - console.log - 2
+let hello1 = console.log("Hello, World!");
+hello1;
+
+//Exersise 1 - console.log - 3
+console.log("Hello, World!");
