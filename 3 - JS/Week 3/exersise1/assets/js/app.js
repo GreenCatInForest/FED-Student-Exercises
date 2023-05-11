58265f608b387b75b5ce6957f20a2e9f1e135d94
Yana Kozarenko <event2016.2017@gmail.com>
2023-05-11 08:16:41 +0100

    more fancy palindrome checking

--- 3 - JS/Week 3/exersise1/assets/js/app.js
+++ 3 - JS/Week 3/exersise1/assets/js/app.js
@@ -3,15 +3,6 @@ let button = document.querySelector("button");
 let resultCheck = document.querySelector("#isPalyndrome");
 
 const checkPalyndrome = (array, reverseArray) => {
-  /*  // checking if arrays works
-
-  for (let i = 0; i < array.length; i++) {
-    console.log(array[i]);
-  }
-  for (let i = 0; i < reverseArray.length; i++) {
-    console.log(reverseArray[i]);
-  } */
-
   for (let i = 0; i < array.length; i++) {
     if (array[i] === reverseArray[i]) {
       resultCheck.innerHTML = "That is palyndrome";
@@ -19,11 +10,41 @@ const checkPalyndrome = (array, reverseArray) => {
   }
 };
 
+/*
+//Checking if a sentence is a palyndrome
+//for example - Poor Dan is in a droop
+//it works
+
 let submitButton = (event) => {
   event.preventDefault();
-  phraseToCheck = inputFromFormPhrase.value;
+  phraseToCheck = inputFromFormPhrase.value.replaceAll(" ", "").toLowerCase();
   array = phraseToCheck.split("");
   reverseArray = array.slice("").reverse("");
   checkPalyndrome(array, reverseArray);
+  console.log(array);
+  console.log(reverseArray);
+};
+button.addEventListener("click", submitButton); */
+
+//Trying to check more fancy palyndrome
+//for example - Poor Dan is,,, in a droop. Sit on a $!potato pan, Otis.
+
+const checkSentencesFromPalyndrome = () => {
+  arrayOfSentences = phraseToCheck
+    .replaceAll(/[,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+    .split(".");
+  console.log(arrayOfSentences);
+};
+
+let submitButton = (event) => {
+  event.preventDefault();
+  phraseToCheck = inputFromFormPhrase.value.replaceAll(" ", "").toLowerCase();
+  checkSentencesFromPalyndrome();
+
+  array = phraseToCheck.split("");
+  reverseArray = array.slice("").reverse("");
+  checkPalyndrome(array, reverseArray);
+  console.log(array);
+  console.log(reverseArray);
 };
 button.addEventListener("click", submitButton);
